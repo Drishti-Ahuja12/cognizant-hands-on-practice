256d8b1185d60d338dca6679fdaf064146bc500e
Drishti-Ahuja12 <driahuja@gmail.com>
2026-06-26 22:15:41 +0530

    add

---
+++ design patterns/DocumentFactory.java
@@ -0,0 +1,8 @@
+abstract class DocumentFactory {
+    public abstract Document createDocument();
+
+    public void openDocument() {
+        Document document = createDocument();
+        document.open();
+    }
+}
---
+++ design patterns/FactoryMethodPatternExample.java
@@ -0,0 +1,11 @@
+public class FactoryMethodPatternExample {
+    public static void main(String[] args) {
+        DocumentFactory wordFactory = new WordDocumentFactory();
+        DocumentFactory pdfFactory = new PdfDocumentFactory();
+        DocumentFactory excelFactory = new ExcelDocumentFactory();
+
+        wordFactory.openDocument();
+        pdfFactory.openDocument();
+        excelFactory.openDocument();
+    }
+}
